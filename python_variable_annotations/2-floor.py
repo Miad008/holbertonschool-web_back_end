7d6715d038910e15fc8dedd5d991d876f23fbbfa
Miad008 <10694@holbertonstudents.com>
2025-07-29 19:44:10 +0300

    Implement floor function with float input and int return type.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for calculating the floor of a float."""
+
+import math
+
+def floor(n: float) -> int:
+    """Returns the floor of a float."""
+    return math.floor(n)
