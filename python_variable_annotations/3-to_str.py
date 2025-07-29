f807e33b71738edeb80750fb313acff470a0496a
Miad008 <10694@holbertonstudents.com>
2025-07-29 19:48:49 +0300

    Add function with float input and return type.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for converting a float to string representation."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float."""
+    return str(n)
