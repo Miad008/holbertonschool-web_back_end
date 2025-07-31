e2a70eba8d285984bb5be4ecb5bba615c6071353
Miad008 <10694@holbertonstudents.com>
2025-07-31 20:23:04 +0300

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -15,6 +15,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
 
     for task in tasks:
         delay = await task
-        bisect.insort(delays, delay) 
+        bisect.insort(delays, delay)  # Insert while keeping list sorted
 
     return delays
