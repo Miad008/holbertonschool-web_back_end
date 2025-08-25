e73eca009941c26fd10bdedb9e874b59740963f8
Miad008 <10694@holbertonstudents.com>
2025-08-25 20:10:34 +0300

    handle empty or undefined startString in cleanSet

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
   const filtered = [...set].filter(value => value.startsWith(startString));
   const trimmed = filtered.map(value => value.slice(startString.length));
   return trimmed.join('-');
